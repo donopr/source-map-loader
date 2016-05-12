02e6e6bf1a4cca178483678b95a76aae8f9cc775
Dmitry <donopr@gmail.com>
2016-05-12 06:33:41 +0500

    Update index.js
    fix DataUrl math

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var baseRegex = "\\s*[@#]\\s*sourceMappingURL\\s*=\\s*([^\\s]*)",
 	// Matches // .... comments
 	regex2 = new RegExp("//"+baseRegex+"($|\n|\r\n?)"),
 	// Matches DataUrls
-	regexDataUrl = /data:[^;\n]+;base64,(.*)/;
+	regexDataUrl = /data:.*=?;base64,(.*)/;
 
 module.exports = function(input, inputMap) {
 	this.cacheable && this.cacheable();
